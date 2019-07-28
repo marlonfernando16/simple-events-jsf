1aaa33d2328da2fd2781dc0f89c65200d153f853
Eliabe Leal <elyabeleal@yahoo.com.br>
2019-07-27 22:18:08 -0300

    tela de cadastro

---
+++ src/main/java/br/edu/ifpb/pweb2/bean/IndexBean.java
@@ -0,0 +1,16 @@
+package br.edu.ifpb.pweb2.bean;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class IndexBean {
+	
+	@PostConstruct
+    public void init() {
+		
+	}
+
+}
